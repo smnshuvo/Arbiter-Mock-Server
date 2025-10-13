8b402348aae6b4ac9a2ed2356813da093a641acf
shuvo-shouman <shuvo.inflack@bkash.com>
2025-10-13 16:08:37 +0600

    Change app package name and icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.spartan_mock_server"
+    namespace = "auravation.arbiter.mock_server"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.spartan_mock_server"
+        applicationId = "auravation.arbiter.mock_server"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/spartan_mock_server/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.spartan_mock_server
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
