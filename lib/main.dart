b14a4c319c58751c276dcca1cfd64a5561bc9351
shuvo-shouman <shuvo.inflack@bkash.com>
2025-10-14 10:23:05 +0600

    Update flutter version

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
             centerTitle: true,
             elevation: 2,
           ),
-          cardTheme: CardTheme(
+          cardTheme: CardThemeData(
             elevation: 2,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(12),
